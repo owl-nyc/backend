5c52f9de2c265cffed2aa76416d1768310c5b76c
Brennan King <brennanhking@uchicago.edu>
2015-08-08 17:48:28 -0400

    adds basic utility functions

---
+++ util.js
@@ -0,0 +1,20 @@
+//calcDistance: calculates distance between two points
+function calcDistance(lat1, lon1, lat2, lon2) {
+    var latDistance = Math.abs(lat1 - lat2);
+    var lonDistance = Math.abs(lon1 - lon2);
+    return Math.sqrt(latDistance + lonDistance);
+}
+//outOfBounds: is the user out of original boundaries specified
+function outOfBounds(origLat, origLon, newLat, newLon, radius) {
+    var distance = calcDistance(origLat, origLon, newLat, newLon);
+    if (distance > radius) {
+        return true;
+    }
+    return false;
+}
+
+//calcVelocity: calculate the users velocity
+function calcVelocity(time1, time2, distance) {
+    return distance / (Math.abs(time1 - time2));
+}
+
