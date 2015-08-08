0839c453f2cb29c024a14bd7d20ae3f4a5e35fee
Brennan King <brennanhking@uchicago.edu>
2015-08-08 16:53:48 -0400

    server receives and sets location

--- server.js
+++ server.js
@@ -8,45 +8,65 @@ app.get('/', function (req, res) {
 	res.send('hi lol');
 });
 
-app.listen(80);
+app.get('/set_state', function (req, res) {
+	//mongodb connection url
+	var url = 'mongodb://localhost:27017/myproject';
+	MongoClient.connect(url, function (err, db) {
+		assert.equal(null, err);
+		setUser(db, req.query, function () {
+			db.close();
+			console.log('success lols');			
+		});
+	});	
+});
 
 
-var insertEntry = function (db, callback) {
-	var collection = db.collection('users');
-
-	collection.insert([ 
-		{a : 1}, {a : 2}, {a: 3}
-	], function (err, result) {
-		assert.equal(err, null);
-		assert.equal(3, result.result.n);
-		assert.equal(3, result.ops.length);
-		console.log('lol inserted those documents chief');
-		callback(result);
-	});
-}
-
-var updateEntry = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('users');
-  // Update document where a is 2, set b equal to 1
-  collection.update({ a : 2 }
-    , { $set: { b : 1 } }, function(err, result) {
-    assert.equal(err, null);
-    assert.equal(1, result.result.n);
-    console.log("Updated the document with the field a equal to 2");
-    callback(result);
-  }); 
-}
-
-//mongodb connection url
-var url = 'mongodb://localhost:27017/myproject';
-MongoClient.connect(url, function (err, db) {
-	assert.equal(null, err);
-	console.log('connected to mongodb server correctly');
-	insertEntry(db, function () {
-		updateEntry(db, function () {
-			db.close();	
+app.get('/get_state', function (req, res) {
+	//mongodb connection url
+	var url = 'mongodb://localhost:27017/myproject';
+	MongoClient.connect(url, function (err, db) {
+		assert.equal(null, err);
+		getUser(db, req.query, res, function () {
+			db.close();
 		});
 	});
 });
 
+app.listen(3000);
+
+//setUser: sets user based on query
+var setUser = function (db, query, callback) {
+	var collection = db.collection('users');
+	var user = query['name'];
+	var results = collection.find({'name': user}).toArray(function(err, users) {
+
+		if (users.length != 0) {
+			console.log('user exists');
+			console.log(users[0]['name']);
+			collection.update( { name : users[0]['name']}, query, callback);
+		} else {
+			console.log('user does not exits');
+			collection.insert(query, callback);
+		}
+
+
+	});
+
+}
+
+//getUser: gets user data on request
+var getUser = function(db, query, res, callback) {
+	var collection = db.collection('users');
+	collection.find({'name' : query['name']}).toArray(function(err, users) {
+		if (users.length != 0) {
+			console.log('found user')
+			console.log(JSON.stringify(users[0]));
+			res.send(JSON.stringify(users[0]));
+		} else {
+			console.log('did not find user');
+			console.log({});
+			res.send({});
+		}
+		callback();
+	})
+}
