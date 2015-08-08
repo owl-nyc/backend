61757276d096a838059eb0735f4dad319823f295
Brennan King <brennanhking@uchicago.edu>
2015-08-08 13:38:27 -0400

    setsup basic mongodb connection

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-var http = require('http');
+var http = require('http'),
+	MongoClient = require('mongodb').MongoClient,
+	assert = require('assert');
+
+//mongodb connection url
+var url = 'mongodb://localhost:27017/myproject';
+MongoClient.connect(url, function (err, db) {
+	assert.equal(null, err);
+	console.log('connected to mongodb server correctly');
+
+	db.close();
+});
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
